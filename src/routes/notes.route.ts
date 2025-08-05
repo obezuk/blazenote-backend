f3e3edbc1c14ddf003b3777de9db8681a5bd2dec
Tim Obezuk <tim@obezuk.com>
2025-08-05 12:35:56 +0800

    Add CRUD, Add CORS, Add D1

--- src/routes/notes.route.ts
+++ src/routes/notes.route.ts
@@ -3,7 +3,12 @@ import { ContextExtended } from "../types";
 
 const notes = new Hono();
 
-notes.get("/", async (ctx: ContextExtended) => {});
+notes.get("/", async (ctx: ContextExtended) => {
+  const db = ctx.env.DB;
+  const notes = await db.prepare("SELECT * FROM note LIMIT 50").run();
+
+  return Response.json(notes.results);
+});
 
 notes.get("/:id", async (ctx: ContextExtended) => {});
 
